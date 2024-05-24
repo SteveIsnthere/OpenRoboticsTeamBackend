7578b0eac7b558ae9864ba7a8a112911e15c87f8
priva <npminitfailed@gmail.com>
2024-05-24 11:02:58 -0700

    fixes

--- models.py
+++ models.py
@@ -6,6 +6,7 @@ import random
 
 faker = Faker()
 
+
 class Project(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(max_length=255, nullable=False)
