236ffd584d0ecd7af46448053de05935b19df879
priva <npminitfailed@gmail.com>
2024-05-25 21:17:00 -0700

    fixes

--- routes/projects.py
+++ routes/projects.py
@@ -16,3 +16,20 @@ router = APIRouter()
 #     if not projects:
 #         raise HTTPException(status_code=404, detail="No projects found")
 #     return projects
+
+@router.get("/get/{newest_first}/{is_completed}", response_model=List[Project])
+async def get_projects(newest_first: bool, is_completed: bool, session: Session = Depends(get_session)):
+    projects = session.exec(select(Project).where(Project.is_completed == is_completed)).all()
+    if newest_first:
+        projects = sorted(projects, key=lambda x: x.time, reverse=True)
+    if not projects:
+        raise HTTPException(status_code=404, detail="No projects found")
+    return projects
+
+
+@router.post("/create")
+async def create_project(project: Project, session: Session = Depends(get_session)):
+    session.add(project)
+    session.commit()
+    session.refresh(project)
+    return True
